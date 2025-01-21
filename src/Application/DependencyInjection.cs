c0214d565861152a29dbc2ecdebcdacb3b3b6754
Bjørn Kristian Punsvik <bjorn.kristian.punsvik@tietoevry.com>
2025-01-21 07:42:46 +0100

    feat: add initial CLI tool with configuration and dependency injection

---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static void AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<ObsidianConfig>(configuration.GetSection(ObsidianConfig.Key));
+    }
+}
---
+++ src/Application/ObsidianConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Application;
+
+public class ObsidianConfig
+{
+    public const string Key = "Obsidian";
+    public string Path { get; init; } = string.Empty;
+}
